259de92390474421cbfc837826b8306863b6605e
Alfredo Gallardo <agallardol@outlook.com>
2025-03-08 20:01:05 -0300

    Refactor project structure and dependencies
    - Moved MCP Server Hello World to a new application
    - Updated project configurations and dependencies
    - Standardized build and lint scripts across projects
    - Removed redundant package files
    - Updated GitHub workflows
    - Migrated to Nx 20.5.0

---
+++ apps/mcp-server-hello-world/src/index.ts
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+import { Server } from "@modelcontextprotocol/sdk/server/index.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import {
+  CallToolRequestSchema,
+  ErrorCode,
+  ListToolsRequestSchema,
+  McpError,
+} from "@modelcontextprotocol/sdk/types.js";
+import { handlers, tools } from "./tools";
+
+const server = new Server({
+  name: "mcp-server-hello-world",
+  version: "1.0.0",
+}, {
+  capabilities: {
+    tools: {}
+  }
+});
+
+const init = async () => {  
+  console.log("Initializing MCP Server Hello World");
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+
+  server.setRequestHandler(ListToolsRequestSchema, async () => {
+    return { tools };
+  });
+
+  server.setRequestHandler(CallToolRequestSchema, async (request) => {
+    const handler = handlers[request.params.name];
+    if (handler) {
+      try {
+        const input = request.params.arguments
+        return await handler(input);
+      } catch (error) {
+        return { toolResult: { error: (error as Error).message }, content: [], isError: true };
+      }
+    }
+    return { toolResult: { error: "Method not found" }, content: [], isError: true };
+  });
+  console.log("MCP Server Hello World initialized");
+};
+
+init();
