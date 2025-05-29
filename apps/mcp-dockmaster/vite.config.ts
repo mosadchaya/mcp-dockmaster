628f05ddcfb9eaa2f1cbcdad0ebf9cc3b6969117
Alfredo Gallardo <agallardol@outlook.com>
2025-05-29 00:31:04 -0400

    fix: wrong output folder for vite. prod ci was failing

--- apps/mcp-dockmaster/vite.config.ts
+++ apps/mcp-dockmaster/vite.config.ts
@@ -15,11 +15,6 @@ export default defineConfig(async () => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    outDir: "../../dist/apps/mcp-dockmaster",
-    emptyOutDir: true,
-  },
-
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
